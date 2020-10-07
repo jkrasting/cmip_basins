2e899b9aff0f4b85b525367204e3c86737dc8b6d
Raphael Dussin <Raphael.Dussin@noaa.gov>
2020-10-06 20:17:16 -0400

    refined polygons

--- basins.py
+++ basins.py
@@ -6,7 +6,7 @@ import xarray as xr
 
 # basins codes used:
 
-ind_SO = 1   # Southern Ocean
+ind_SO = 1  # Southern Ocean
 ind_ATL = 2  # Atlantic Ocean
 ind_PAC = 3  # Pacific Ocean
 ind_ARC = 4  # Arctic Ocean
@@ -15,9 +15,9 @@ ind_MED = 6  # Mediterranean Sea
 ind_BLK = 7  # Black Sea
 ind_HUD = 8  # Hudson Bay
 ind_BAL = 9  # Baltic Sea
-ind_RED = 10 # Red Sea
-ind_PER = 11 # Persian Gulf
-ind_tmp = 99 # to merge 2 parts of Pacific
+ind_RED = 10  # Red Sea
+ind_PER = 11  # Persian Gulf
+ind_tmp = 99  # to merge 2 parts of Pacific
 
 
 # polygon points:
@@ -46,35 +46,70 @@ SO = np.array(
 
 ATL = np.array(
     [
-        [-49.0, 80.0],  # lab sea greenland
-        [-47.3, 65.0],  # greenland
+        [-42.0, 66.5],  # greenland
+        [-41.0, 66.0],
+        [-40.0, 65.0],  # greenland
         [-18.6, 65.0],  # iceland
         [15.1, 65.0],  # sweden
-        [9.16, 56.4],  # denmark
+        [14.49, 56.99],  # South Sweden
+        [12.56, 56.04],  # Helsinger
+        [12.29, 55.63],  # Copenhagen
+        [11.60, 55.49],  # Zealand
+        [10.72, 55.32],  # Funen
+        [9.23, 54.79],  # Flensburg
+        [9.82, 53.50],  # Hamburg
         [2.3, 47.1],  # bourges
+        [-0.04, 43.07],  # lourdes
         [-5.6, 40.0],  # caceres
         [-5.6, 30.1],  # morocco
         [cape_town[0], 7],
+        [19.0, -33.0],  # just inland from cape town
         [cape_town[0], cape_town[1]],
         [-68.1, cape_town[1]],
         [-68.1, 7],  # venezuela
-        [-79.7, 9.1],  # gabon
+        [-75.68, 6.22],  # Medellin, Columbia
+        [-77.68, 8.12],  # Yaviza, Panama
+        [-79.08, 9.32],  # Guna Yala, Panama
+        [-81.29, 8.31],  # Veraguas, Panama
+        [-83.76, 9.82],  # Costa Rica
+        [-86.09, 12.14],  # Nicaragua
+        [-88.42, 14.12],  # Honduras/El Sal
+        [-90.77, 15.10],  # Quiche, Guatemala
+        [-96.04, 17.56],  # Oaxaca, MX
         [-99.1, 19.4],  # mexico city
         [-106.7, 35.1],  # albequerque
-        [-66.9, 52.9],  # labrador city
-        [-70.4, 65.0],  # baffin island 1
-        [-72.7, 69.9],  # nunavut
-        [-85.9, 80.0],  # eureka
+        [-76.46, 49.60],  # Baie James. QC
+        [-71.71, 60.72],  # Riviere Koksoak, QC
+        [-70.83, 63.06],  # Kimmirut, Nunavut
+        [-69.62, 64.14],  #
+        [-68.61, 66.46],  # Kipisa, NU
+        [-68.20, 67.72],  # Baffin island
+        [-62.02, 66.71],  # Cape Dyer
+        [-52.46, 68.29],  # Davis strait, Greenland side
     ]
 )
 
 PAC1 = np.array(
     [
-        [-180.0, 65.0],
+        [-180.0, 67.0],
+        [-179.84, 66.85],  # Russia
+        [-177.52, 66.69],  # Russia
+        [-174.10, 66.02],  # Russia
+        [-169.86, 66.09],  # Naukan, Russia
+        [-169.06, 65.80],  # Big Diomede
+        [-167.97, 65.61],  # Wales, AK
         [-140.0, 65.0],
         [-106.7, 35.1],  # albequerque
         [-99.1, 19.4],  # mexico city
-        [-79.7, 9.1],  # gabon
+        [-96.04, 17.56],  # Oaxaca, MX
+        [-90.77, 15.10],  # Quiche, Guatemala
+        [-88.42, 14.12],  # Honduras/El Sal
+        [-86.09, 12.14],  # Nicaragua
+        [-83.76, 9.82],  # Costa Rica
+        [-81.29, 8.31],  # Veraguas, Panama
+        [-79.08, 9.32],  # Guna Yala, Panama
+        [-77.68, 8.12],  # Yaviza, Panama
+        [-75.68, 6.22],  # Medellin, Columbia
         [-68.1, 7],  # venezuela
         [-68.1, melbourne[1]],
         [-180.0, melbourne[1]],
@@ -84,14 +119,25 @@ PAC1 = np.array(
 PAC2 = np.array(
     [
         [98.8, 65.0],
-        [180.0, 65.0],
+        [180.0, 67.0],
         [180.0, melbourne[1]],
         [melbourne[0], melbourne[1]],
         [melbourne[0], -30],
         [123.1, -10.7],  # rote island
-        [105.2, -6.6],  # panaitan island
-        [101.6, 3.1],  # kuala lumpur
-        [98.8, 9.1],
+        [119.95, -9.75],  # Sumba Regency
+        [117.31, -8.73],  # West Nusa
+        [116.33, -8.60],  # Lombok
+        [115.18, -8.45],  # Bali
+        [113.95, -8.09],  # east Java
+        [106.41, -6.56],  # west Java
+        [105.39, -5.25],  # south Sumatra
+        [102.19, 0.07],  # Sumatra
+        [103.85, 1.69],  # singapore
+        [98.86, 9.03],  # Thailand
+        [98.8, 9.1],  # surat thani
+        [99.07, 16.73],  # Tak, Thailand
+        [98.8, 23.0],  # mangmao
+        [95.33, 27.42],  # Tinkusia, india
     ]
 )
 
@@ -99,50 +145,84 @@ ARC = np.array(
     [
         [-180.0, 90.0],
         [180.0, 90.0],
-        [180.0, 65.0],
+        [180.0, 67.0],
         [98.8, 65.0],  # krasnoyarsk
-        [35.0, 60.0],  # leningrad
-        [25.0, 69.0],  # karasjok
-        [15.1, 65.0],  # st petersburg
-        [-47.3, 65.0],  # sermersooq 1
-        [-49.0, 80.0],  # sermersooq 2
-        [-85.9, 80.0],  # ellesmere island
-        [-72.7, 69.9],  # baffin island 2
-        [-70.4, 65.0],  # baffin island 1
-        [-84.7, 64.8],  # coral harbour
-        [-90.0, 66.0],  # keewatin
+        [35.93, 60.93],  # Russia
+        [21.30, 68.04],  # North Sweden
+        [15.1, 65.0],  # sweden
+        [-18.6, 65.0],  # iceland
+        [-40.0, 65.0],  # greenland
+        [-41.0, 66.0],
+        [-42.0, 66.5],  # greenland
+        [-52.46, 68.29],  # Davis strait, Greenland side
+        [-62.02, 66.71],  # Cape Dyer
+        [-68.20, 67.72],  # Baffin island
+        [-78.18, 71.13],  # Baffin island
+        [-83.69, 71.03],  # Baffin island
+        [-84.35, 68.07],  # Baffin, NU
+        [-86.93, 66.88],  # Keewatin, NU
+        [-93.19, 66.34],  # Keewatin, NU
+        [-100.53, 64.47],  # Keewatin, NU
         [-140.0, 65.0],
-        [-180.0, 65.0],
+        [-167.97, 65.61],  # Wales, AK
+        [-169.06, 65.80],  # Big Diomede
+        [-169.86, 66.09],  # Naukan, Russia
+        [-174.10, 66.02],  # Russia
+        [-177.52, 66.69],  # Russia
+        [-179.84, 66.85],  # Russia
+        [-180.0, 67.0],
     ]
 )
 
 HUD = np.array(
     [
-        [-90.0, 66.0],  # keewatin
-        [-84.7, 64.8],  # coral harbour
-        [-70.4, 65.0],  # baffin island
-        [-66.9, 52.9],  # labrador city
-        [-78.1, 48.0],  # val-d'or
-        [-107.2, 59.3],  # fond-du-lac
+        [-70.83, 63.06],  # Kimmirut, Nunavut
+        [-71.71, 60.72],  # Riviere Koksoak, QC
+        [-76.46, 49.60],  # Baie James. QC
+        [-87.01, 52.01],  # Ontario
+        [-99.04, 57.21],  # Manitoba
+        [-100.53, 64.47],  # Keewatin, NU
+        [-93.19, 66.34],  # Keewatin, NU
+        [-86.93, 66.88],  # Keewatin, NU
+        [-84.35, 68.07],  # Baffin, NU
+        [-83.69, 71.03],  # Baffin island
+        [-78.18, 71.13],  # Baffin island
+        [-68.20, 67.72],  # Baffin island
+        [-68.61, 66.46],  # Kipisa, NU
+        [-69.62, 64.14],  #
     ]
 )
 
 IND = np.array(
     [
         [cape_town[0], 7],
-        [43.8, 12.4],  # bab al-mandab
+        [36.0, 15.2],  # eritra
+        [43.17, 12.50],  # bab al-mandab
+        [43.60, 12.84],  # bab al-mandab
         [57.4, 22.9],  # nizwa
         [55.6, 24.1],  # al ain
         [57.0, 27.1],  # minab
         [62.1, 25.4],  # turbat
+        [95.33, 27.42],  # Tinkusia, india
         [98.8, 23.0],  # mangmao
+        [99.07, 16.73],  # Tak, Thailand
         [98.8, 9.1],  # surat thani
-        [105.2, -6.6],  # panaitan island
+        [98.86, 9.03],  # Thailand
+        [103.85, 1.69],  # singapore
+        [102.19, 0.07],  # Sumatra
+        [105.39, -5.25],  # south Sumatra
+        [106.41, -6.56],  # west Java
+        [113.95, -8.09],  # east Java
+        [115.18, -8.45],  # Bali
+        [116.33, -8.60],  # Lombok
+        [117.31, -8.73],  # West Nusa
+        [119.95, -9.75],  # Sumba Regency
         [123.1, -10.7],  # rote island
         [melbourne[0], -30],
         [perth[0], -30],
         [perth[0], cape_town[1]],
         [cape_town[0], cape_town[1]],
+        [19.0, -33.0],  # just inland from cape town
     ]
 )
 
@@ -151,31 +231,42 @@ MED = np.array(
         [2.3, 47.1],  # bourges
         [11.6, 48.0],  # munich
         [28.7, 41.0],  # istanbul
+        [33.02, 40.03],  # ankara
         [37.3, 37.1],  # gaziantep
         [36.8, 30.6],  # jordan
         [31.2, 30.1],  # cairo
         [-5.6, 30.1],  # morocco
         [-5.6, 40.0],  # caceres
+        [-0.04, 43.07],  # lourdes
     ]
 )
 
 BLK = np.array(
     [
         [28.7, 41.0],  # istanbul
-        [23.2, 42.7],  # sofia
+        [24.78, 43.51],  # pleven
         [30.1, 48.7],  # uman
         [44.2, 48.7],  # volograd
         [43.9, 36.2],  # erbil
+        [33.02, 40.03],  # ankara
     ]
 )
 
 BAL = np.array(
     [
-        [15.1, 65.0],  # st petersburg
-        [25.0, 69.0],  # karasjok
-        [35.0, 60.0],  # leningrad
-        [11.6, 48.0],  # munich
-        [2.3, 47.1],  # bourges
+        [9.82, 53.50],  # Hamburg
+        [9.23, 54.79],  # Flensburg
+        [10.72, 55.32],  # Funen
+        [11.60, 55.49],  # Zealand
+        [12.29, 55.63],  # Copenhagen
+        [12.56, 56.04],  # Helsinger
+        [14.49, 56.99],  # South Sweden
+        [15.1, 65.0],  # sweden
+        [21.30, 68.04],  # North Sweden
+        [35.93, 60.93],  # Russia
+        [30.42, 59.85],  # st petersburg
+        [21.06, 52.42],  # warsaw
+        [12.18, 51.24],  # Liepzig
     ]
 )
 
@@ -183,7 +274,8 @@ RED = np.array(
     [
         [36.8, 30.6],  # jordan
         [44.2, 18.5],  # yadamah
-        [43.8, 12.4],  # bab al-mandab
+        [43.60, 12.84],  # bab al-mandab
+        [43.17, 12.50],  # bab al-mandab
         [36.0, 15.2],  # eritra
         [30.5, 25.4],  # kharga
         [31.2, 30.1],  # cairo
@@ -203,23 +295,63 @@ PER = np.array(
 )
 
 
-
 basins = regionmask.Regions(
     [SO, ATL, PAC1, PAC2, ARC, IND, MED, BLK, HUD, BAL, RED, PER],
-    [ind_SO, ind_ATL, ind_tmp, ind_PAC, ind_ARC, ind_IND, ind_MED,
-     ind_BLK, ind_HUD, ind_BAL, ind_RED, ind_PER]
+    [
+        ind_SO,
+        ind_ATL,
+        ind_tmp,
+        ind_PAC,
+        ind_ARC,
+        ind_IND,
+        ind_MED,
+        ind_BLK,
+        ind_HUD,
+        ind_BAL,
+        ind_RED,
+        ind_PER,
+    ],
 )
 
 
+def generate_basin_codes(grid, lon="geolon", lat="geolat", mask="wet", persian=True):
+    """generate CMIP6 basin codes
 
-def generate_basin_codes(grid, lon="geolon", lat="geolat", mask="wet"):
+    Parameters
+    ----------
+
+    grid : xarray.Dataset
+        contains grid information: lon, lat, [mask]
+
+    lon : str
+        name of longitude array in grid
+
+    lat : str
+        name of latitude array in grid
+
+    mask : str
+        name of mask array in grid
+
+    persian : bool (default=True)
+        have persian gulf as separate code, else merge with indian ocean
+
+    Returns
+    -------
+
+    codes : xr.DataArray
+        basin codes
+    """
 
     lon_raw = grid[lon]
-    lon = xr.where(lon_raw < -180, lon_raw+360., lon_raw)
+    lon = xr.where(lon_raw < -180, lon_raw + 360.0, lon_raw)
     lat = grid[lat]
 
     codes = basins.mask(lon, lat)
-    codes = xr.where(codes == ind_tmp, ind_PAC, codes) # join PAC1 and PAC2
+    codes = xr.where(codes == ind_tmp, ind_PAC, codes)  # join PAC1 and PAC2
+
+    # add persian gulf to indian ocean
+    if not persian:
+        codes = xr.where(codes == ind_PER, ind_IND, codes)
 
     if mask in grid:
         masked = grid[mask].values
@@ -227,12 +359,11 @@ def generate_basin_codes(grid, lon="geolon", lat="geolat", mask="wet"):
     else:
         codes.fillna(0)
 
-    codes['lon'] = lon_raw
+    codes["lon"] = lon_raw
     return codes
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     # define 1x1 grid
     lon = np.arange(0.5, 360.5)
@@ -242,25 +373,31 @@ if __name__ == '__main__':
     fig = plt.figure(figsize=(25, 10))
     ax = plt.subplot(111, projection=ccrs.PlateCarree())
     mask = basins.mask(lon, lat)
-    mask =xr.where(mask == ind_tmp, ind_PAC, mask)
+    mask = xr.where(mask == ind_tmp, ind_PAC, mask)
 
     # pcolormesh does not handle NaNs, requires masked array
     mask_ma = np.ma.masked_invalid(mask)
-    h = ax.pcolormesh(lon, lat, mask_ma, transform=ccrs.PlateCarree(), cmap="jet",)
-    basins.plot_regions(ax=ax, add_label=False)
+    h = ax.pcolormesh(
+        lon,
+        lat,
+        mask_ma,
+        transform=ccrs.PlateCarree(),
+        cmap="jet",
+    )
+    basins.plot_regions(ax=ax, add_label=False, line_kws=dict(color="r"))
     ax.coastlines()
 
     plt.show()
-    #plt.savefig("basins.png")
+    # plt.savefig("basins.png")
 
     grid = xr.Dataset()
-    grid['lon'] = xr.DataArray(lon, dims=('lon'))
-    grid['lat'] = xr.DataArray(lat, dims=('lat'))
-    codes = generate_basin_codes(grid, lon='lon', lat='lat')
+    grid["lon"] = xr.DataArray(lon, dims=("lon"))
+    grid["lat"] = xr.DataArray(lat, dims=("lat"))
+    codes = generate_basin_codes(grid, lon="lon", lat="lat")
 
     plt.figure()
     ax = plt.subplot(111, projection=ccrs.PlateCarree())
-    codes.plot(ax=ax, x='lon',y='lat', cmap='tab20')
+    codes.plot(ax=ax, x="lon", y="lat", cmap="tab20")
     ax.coastlines()
 
     plt.show()
