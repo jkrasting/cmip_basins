feba81b9369b77518a54878b9dbb26ea900c423f
John Krasting <John.Krasting@noaa.gov>
2020-10-08 10:03:37 -0400

    Applied black formatting

--- cmip_basins/tests/test_basins.py
+++ cmip_basins/tests/test_basins.py
@@ -3,10 +3,12 @@ import xarray as xr
 import numpy as np
 from cmip_basins import basins
 
+
 @pytest.mark.parametrize("persian", [True, False])
 def test_generate_basin_codes(persian):
 
     from cmip_basins.basins import generate_basin_codes
+
     # define 1x1 grid
     lon = np.arange(0.5, 360.5)
     lat = np.arange(89.5, -90.5, -1)
@@ -17,6 +19,6 @@ def test_generate_basin_codes(persian):
     codes = generate_basin_codes(grid, lon="lon", lat="lat", persian=persian)
 
     assert isinstance(codes, xr.core.dataarray.DataArray)
-    codes = codes.fillna(0.)
-    assert (codes.values.min() == 0)
-    assert (codes.values.max() in [10,11])
+    codes = codes.fillna(0.0)
+    assert codes.values.min() == 0
+    assert codes.values.max() in [10, 11]
