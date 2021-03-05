b6afaf7b8613c0cf4729bb8c100db25e03b73079
John Krasting <John.Krasting@noaa.gov>
2021-03-05 16:09:08 -0500

    Updated files for packaging

--- setup.py
+++ setup.py
@@ -1,15 +1,5 @@
-""" setup for cmip_basins """
+""" setup script """
 import setuptools
 
-setuptools.setup(
-    name="cmip_basins",
-    version="0.0.1",
-    author="",
-    author_email="",
-    description=(""),
-    license="",
-    keywords="",
-    url="",
-    packages=["cmip_basins"],
-    scripts=["cmip_basins/basins.py"],
-)
+setuptools.setup()
+
